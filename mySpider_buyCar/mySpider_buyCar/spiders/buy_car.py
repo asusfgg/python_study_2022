92bfd29032aaddf7385788e8ec9937e8f4ece419
asusfgg <asusfgg@hotmail.com>
2022-03-25 11:28:26 +0800

    3-25-11:28

--- mySpider_buyCar/mySpider_buyCar/spiders/buy_car.py
+++ mySpider_buyCar/mySpider_buyCar/spiders/buy_car.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2022-03-24 15:33:40
-LastEditTime: 2022-03-24 15:44:18
+LastEditTime: 2022-03-25 11:23:13
 LastEditors: Please set LastEditors
 Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 FilePath: \python_study_2022\mySpider_buyCar\mySpider_buyCar\spiders\buy_car.py
@@ -22,9 +22,14 @@ class BuyCarSpider(scrapy.Spider):
         name_list = response.xpath('//div[@class="main-title"]/a/text()')
         # print(name_list)
         # 因为是个列表，则可以遍历
-        for name in name_list:
-            # .extract提取对象的data属性值
-            print(name.extract())
-        print('================================================================')
-
+        # for name in name_list:
+        # .extract提取对象的data属性值
+        #     print(name.extract())
+        price_list = response.xpath(
+            '//span[@class="lever-price red"]/span/text()')
+        for i in range(len(name_list)):
+            # 打印索引下标
+            # print(i)
+            # 对应打印
+            print(name_list[i].extract(), price_list[i].extract())
         pass
